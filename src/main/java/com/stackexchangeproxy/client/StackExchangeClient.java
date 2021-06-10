29a7b4b3040b490354da364e952c7a132a406de5
Maksim_Smolnikov <maksim_smolnikov@epam.com>
2021-06-10 10:43:36 +0400

    add javadoc for client

--- src/main/java/com/stackexchangeproxy/client/StackExchangeClient.java
+++ src/main/java/com/stackexchangeproxy/client/StackExchangeClient.java
@@ -3,6 +3,10 @@ package com.stackexchangeproxy.client;
 import com.stackexchangeproxy.client.model.SearchItem;
 import reactor.core.publisher.Flux;
 
+/**
+ * Client for work with <a href="https://api.stackexchange.com/docs/search#&order=desc&sort=
+ * activity&filter=default&site=stackoverflow&run=true">stackexchange</a>
+ */
 public interface StackExchangeClient {
 
   Flux<SearchItem> search(String query);
